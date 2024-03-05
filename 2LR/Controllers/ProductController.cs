6925ee7d6d3e73d031921994a6f8c531d964a322
Кузя <@LAPTOP-151DFMK3>
2024-03-05 17:18:32 +0300

    Add project files.

---
+++ 2LR/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using _2LR.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2LR.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> _products = new List<Product>();
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _products.Add(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Clear()
+        {
+            _products.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ 2LR/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _2LR.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        public string ?Name { get; set; }
+
+        [Required(ErrorMessage = "Введите цену")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Введите положительное число")]
+        public double? Price { get; set; }
+
+        [Required(ErrorMessage = "Введите вес")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Введите положительное число")]
+        public double? Weight { get; set; }
+    }
+
+}
