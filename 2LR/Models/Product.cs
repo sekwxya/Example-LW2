6b0dfa7122a9954bf544eb4f8b534277af0a481e
sekwxya <tima.latipov.09@gmail.com>
2024-03-12 00:44:56 +0300

    Обновление под базу данных о ноутбуках

--- 2LR/Models/Product.cs
+++ 2LR/Models/Product.cs
@@ -6,16 +6,18 @@ namespace _2LR.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Введите имя")]
-        public string? Name { get; set; }
+        [Required(ErrorMessage = "Введите бренд")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Длина названия бренда должна быть от 1 до 50 символов")]
+        public string? Brend { get; set; }
+
+        [Required(ErrorMessage = "Введите модель")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Длина названия модели должна быть от 1 до 50 символов")]
+        public string? ItemModel { get; set; }
 
         [Required(ErrorMessage = "Введите цену")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Введите положительное число")]
         public double? Price { get; set; }
 
-        [Required(ErrorMessage = "Введите вес")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Введите положительное число")]
-        public double? Weight { get; set; }
     }
 
 }
